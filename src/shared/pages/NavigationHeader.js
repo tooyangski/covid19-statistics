b5b61062804dd23a907e9fc01dffa882c410fbef
Aubrey Ivan Apungan <apungan.aubreyivansunga@gmail.com>
2020-10-20 23:53:13 +0800

    responsive navigation header

--- src/shared/pages/NavigationHeader.js
+++ src/shared/pages/NavigationHeader.js
@@ -10,18 +10,18 @@ import {
 
 const NavigationHeader = () => {
   return (
-    <Navbar bg="dark" variant="dark">
-      <Navbar.Toggle aria-controls="basic-navbar-nav" />
-      <Navbar.Collapse id="basic-navbar-nav">
+    <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+      <Navbar.Brand href="/">Manulife</Navbar.Brand>
+      <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+      <Navbar.Collapse id="responsive-navbar-nav">
         <Nav className="mr-auto">
-          <Nav.Link href="#home">Home</Nav.Link>
-          <Nav.Link href="#link">About</Nav.Link>
-          <NavDropdown title="Account" id="basic-nav-dropdown">
-            <NavDropdown.Item href="#action/3.1">Subscription</NavDropdown.Item>
+          <Nav.Link href="/">Home</Nav.Link>
+          <Nav.Link href="#contactus">Contact Us</Nav.Link>
+          <NavDropdown title="Account" id="collasible-nav-dropdown">
             <NavDropdown.Item href="#action/3.1">Profile</NavDropdown.Item>
-            <NavDropdown.Item href="#action/3.1">Settings</NavDropdown.Item>
+            <NavDropdown.Item href="#action/3.2">Settings</NavDropdown.Item>
+            <NavDropdown.Item href="#action/3.3">Subscription</NavDropdown.Item>
             <NavDropdown.Divider />
-
             <NavDropdown.Item href="#action/3.4">
               Report an issue?
             </NavDropdown.Item>
