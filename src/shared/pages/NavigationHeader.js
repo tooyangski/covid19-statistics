519c080f164fb60ed62b60d993dd514d0313b9d8
Aubrey Ivan Apungan <apungan.aubreyivansunga@gmail.com>
2020-10-20 23:38:27 +0800

    minor fixes

--- src/shared/pages/NavigationHeader.js
+++ src/shared/pages/NavigationHeader.js
@@ -16,10 +16,12 @@ const NavigationHeader = () => {
         <Nav className="mr-auto">
           <Nav.Link href="#home">Home</Nav.Link>
           <Nav.Link href="#link">About</Nav.Link>
-          <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-            <NavDropdown.Item href="#action/3.1">Inquire</NavDropdown.Item>
-            <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+          <NavDropdown title="Account" id="basic-nav-dropdown">
+            <NavDropdown.Item href="#action/3.1">Subscription</NavDropdown.Item>
+            <NavDropdown.Item href="#action/3.1">Profile</NavDropdown.Item>
+            <NavDropdown.Item href="#action/3.1">Settings</NavDropdown.Item>
             <NavDropdown.Divider />
+
             <NavDropdown.Item href="#action/3.4">
               Report an issue?
             </NavDropdown.Item>
